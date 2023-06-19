ad59f25fd149550f31ad6a4a76df66eb27cc1159
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-20 00:21:19 +0900

    회원가입, 로그인, 아이디/닉네임 중복 서비스단 수정

--- solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/service/UserService.java
+++ solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/service/UserService.java
@@ -20,7 +20,7 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void save(UserDto userDto){
+    public SignupResponse save(UserDto userDto){
         User user = User.builder()
                 .id(userDto.getId())
                 .password(passwordEncoder.encode(userDto.getPassword()))
@@ -29,18 +29,20 @@ public class UserService {
                 .build();
 
         userRepository.save(user);
+
+        return SignupResponse.from(user.getId(), user.getNickname(), user.getDate());
     }
 
-
-    public String login(String id, String password){ // 로그인
-        Optional<User> user = userRepository.findById(id);
-        if(user.isPresent() && passwordEncoder.matches(password, user.get().getPassword())){
-            return "true";
-        }
-        else{
-            return "false";
-        }
-    }
+//
+//    public String login(String id, String password){ // 로그인
+//        Optional<User> user = userRepository.findById(id);
+//        if(user.isPresent() && passwordEncoder.matches(password, user.get().getPassword())){
+//            return "true";
+//        }
+//        else{
+//            return "false";
+//        }
+//    }
 
     public boolean checkLogin(String id, String password) {
         Optional<User> user = userRepository.findById(id);
@@ -50,15 +52,12 @@ public class UserService {
         return passwordEncoder.matches(password, user.get().getPassword());
     }
 
-
     public boolean checkDuplicateNickname(String username) {
-        return userRepository.findByNickname(username).isPresent();
+        return !userRepository.findByNickname(username).isPresent();
     }
 
 
     public boolean checkDuplicateId(String id){
-        return userRepository.findById(id).isPresent();
+        return !userRepository.findById(id).isPresent();
     }
-
-
 }
