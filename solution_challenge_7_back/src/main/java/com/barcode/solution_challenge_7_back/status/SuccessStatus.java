d1af238907860c471df8e06c1042bee9a46b78db
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-19 22:39:31 +0900

    SuccessStatus 추가

--- solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/status/SuccessStatus.java
+++ solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/status/SuccessStatus.java
@@ -1,2 +1,27 @@
-package com.barcode.solution_challenge_7_back.status;public class SuccessStatus {
+package com.barcode.solution_challenge_7_back.status;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessStatus {
+
+    /*
+    user
+     */
+    SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 완료되었습니다."),
+
+
+
+
+    LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공!"),
+
+    CERTIFICATION_SUCCESS(HttpStatus.OK, "유저 인증 성공")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
 }
