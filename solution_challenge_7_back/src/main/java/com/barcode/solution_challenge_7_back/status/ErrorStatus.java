3367c954264621c663b1f4c71f89b7f794bdca51
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-19 23:22:46 +0900

    ErrorStatus 아이디, 비밀번호 없는 유저 에러메세지  추가

--- solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/status/ErrorStatus.java
+++ solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/status/ErrorStatus.java
@@ -15,7 +15,7 @@ public enum ErrorStatus {
     VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     NOT_LOGIN_STATUS(HttpStatus.BAD_REQUEST, "로그인 되어있지 않습니다."),
     VALIDATION_REQUEST_MISSING_EXCEPTION(HttpStatus.BAD_REQUEST, "요청값이 입력되지 않았습니다."),
-    USER_CERTIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "해당 이메일이나 비밀번호를 가진 유저가 존재하지 않습니다."),
+    USER_CERTIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "해당 아이디나 비밀번호를 가진 유저가 존재하지 않습니다."),
     USER_NOT_JOIN(HttpStatus.FORBIDDEN, "해당 사용자가 존재하지 않습니다."),
 
 
