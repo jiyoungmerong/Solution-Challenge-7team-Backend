a9e8690a42c092c19c28f2407937999b7a6bac65
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-19 23:23:16 +0900

    회원가입, 로그인 api 수정

--- solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/controller/UserController.java
+++ solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.barcode.solution_challenge_7_back.controller;
 
 import com.barcode.solution_challenge_7_back.domain.User;
-import com.barcode.solution_challenge_7_back.domain.dto.ApiResponse;
+import com.barcode.solution_challenge_7_back.domain.dto.ApiResponseDto;
+import com.barcode.solution_challenge_7_back.domain.dto.UserDto;
 import com.barcode.solution_challenge_7_back.domain.request.LoginRequest;
-import com.barcode.solution_challenge_7_back.domain.request.SignupRequest;
 import com.barcode.solution_challenge_7_back.domain.response.SignupResponse;
-import com.barcode.solution_challenge_7_back.exception.status.SuccessStatus;
 import com.barcode.solution_challenge_7_back.repository.UserRepository;
 import com.barcode.solution_challenge_7_back.service.UserService;
+import com.barcode.solution_challenge_7_back.status.ErrorStatus;
+import com.barcode.solution_challenge_7_back.status.SuccessStatus;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponses;
@@ -23,8 +24,6 @@ import java.util.Optional;
 public class UserController {
     private final UserService userService;
     private final UserRepository userRepository;
-
-
     @ApiOperation(value = "회원가입", notes = "새로운 사용자를 회원으로 등록합니다.")
     @ApiResponses(value = {
             @io.swagger.annotations.ApiResponse(code = 200, message = "회원가입 성공"),
@@ -32,8 +31,19 @@ public class UserController {
             @io.swagger.annotations.ApiResponse(code = 500, message = "서버 오류")
     })
     @PostMapping("/users/new-user") // 회원가입
-    public ApiResponse<SignupResponse> join(@RequestBody SignupRequest request) {
-        return ApiResponse.success(SuccessStatus.SIGNUP_SUCCESS, userService.create(request));
+    public ApiResponseDto<SignupResponse> join(@RequestBody UserDto userDto) {
+        try {
+            if(userService.checkDuplicateId(userDto.getId())){ // 아이디 중복 시
+                return ApiResponseDto.error(ErrorStatus.CONFLICT_ID_EXCEPTION);
+
+            } else if (userService.checkDuplicateNickname(userDto.getNickname())) { // 닉네임 중복시
+                return ApiResponseDto.error(ErrorStatus.CONFLICT_NICKNAME_EXCEPTION);
+            }
+            userService.save(userDto);
+            return ApiResponseDto.success(SuccessStatus.SIGNUP_SUCCESS); // 회원가입 성공
+        } catch (Exception e) { // 그 밖의 예외 발생시
+            return ApiResponseDto.error(ErrorStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @ApiOperation(value = "로그인", notes = "아이디, 비밀번호로 로그인")
@@ -43,9 +53,9 @@ public class UserController {
             @io.swagger.annotations.ApiResponse(code = 500, message = "서버 오류")
     })
     @PostMapping("/login") // 로그인
-    public ResponseEntity<Boolean> login(@RequestBody LoginRequest request) {
+    public ApiResponseDto<String> login(@RequestBody LoginRequest request) {
         boolean isAuthenticated = userService.checkLogin(request.getId(), request.getPassword());
-        return isAuthenticated ? ResponseEntity.ok(true) : ResponseEntity.badRequest().body(false);
+        return isAuthenticated ? ApiResponseDto.success(SuccessStatus.LOGIN_SUCCESS) : ApiResponseDto.error(ErrorStatus.USER_CERTIFICATION_FAILED);
     }
 
     @ApiOperation(value = "아이디 중복 확인", notes = "해당 id가 이미 존재한다면 true, 존재하지 않다면 false를 반환합니다.")
