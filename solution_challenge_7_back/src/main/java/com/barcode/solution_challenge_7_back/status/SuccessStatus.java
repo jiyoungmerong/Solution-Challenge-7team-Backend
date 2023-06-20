b17da21865aaf653d5a77490941dbf04fcddf8fa
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-20 19:09:18 +0900

    요일 가져오기 successStatus 추가

--- solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/status/SuccessStatus.java
+++ solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/status/SuccessStatus.java
@@ -8,18 +8,18 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessStatus {
-
-    /*
-    user
-     */
     SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 완료되었습니다."),
 
+    CREATE_ID_SUCCESS(HttpStatus.OK, "사용 가능한 아이디입니다."),
 
+    CREATE_NICKNAME_SUCCESS(HttpStatus.OK, "사용 가능한 닉네임입니다."),
 
 
     LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공!"),
 
-    CERTIFICATION_SUCCESS(HttpStatus.OK, "유저 인증 성공")
+    CERTIFICATION_SUCCESS(HttpStatus.OK, "유저 인증 성공"),
+
+    BRING_DATE_SUCCESS(HttpStatus.OK, "사용자 지정 요일 가져오기 성공")
     ;
 
     private final HttpStatus httpStatus;
