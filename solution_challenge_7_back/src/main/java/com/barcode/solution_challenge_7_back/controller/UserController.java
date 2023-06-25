b58d9140dfbda739fc1ce5a984b08aa547208c7f
jiyoungmerong <jiyoungmerong@gmail.com>
2023-06-26 00:41:39 +0900

    User관련 api 최종수정 완료

--- solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/controller/UserController.java
+++ solution_challenge_7_back/src/main/java/com/barcode/solution_challenge_7_back/controller/UserController.java
@@ -15,10 +15,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
 
 @RestController
@@ -26,6 +23,7 @@ import java.util.Optional;
 public class UserController {
     private final UserService userService;
     private final UserRepository userRepository;
+
     @ApiOperation(value = "회원가입", notes = "새로운 사용자를 회원으로 등록합니다.")
     @ApiResponses(value = {
             @io.swagger.annotations.ApiResponse(code = 200, message = "회원가입 성공"),
@@ -35,12 +33,6 @@ public class UserController {
     @PostMapping("/users/new-user") // 회원가입
     public ApiResponseDto<SignupResponse> join(@RequestBody UserDto userDto) {
         try {
-//            if(userService.checkDuplicateId(userDto.getId())){ // 아이디 중복 시
-//                return ApiResponseDto.error(ErrorStatus.CONFLICT_ID_EXCEPTION);
-//
-//            } else if (userService.checkDuplicateNickname(userDto.getNickname())) { // 닉네임 중복시
-//                return ApiResponseDto.error(ErrorStatus.CONFLICT_NICKNAME_EXCEPTION);
-//            }
             return ApiResponseDto.success(SuccessStatus.SIGNUP_SUCCESS, userService.save(userDto)); // 회원가입 성공
         } catch (Exception e) { // 그 밖의 예외 발생시
             return ApiResponseDto.error(ErrorStatus.INTERNAL_SERVER_ERROR);
@@ -61,39 +53,19 @@ public class UserController {
 
     @ApiOperation(value = "아이디 중복 확인", notes = "해당 id가 이미 존재한다면 true, 존재하지 않다면 false를 반환합니다.")
     @ApiImplicitParam(name = "id", value = "사용자가 생성한 id")
-    @GetMapping("/checkDuplicateId/{id}") // 아이디 중복 확인
-    public ApiResponseDto<String> checkDuplicateId(@PathVariable String id) {
-        boolean isDuplicateId = userService.checkDuplicateId(id);
-        return isDuplicateId ? ApiResponseDto.success(SuccessStatus.CREATE_ID_SUCCESS, id) : ApiResponseDto.error(ErrorStatus.CONFLICT_ID_EXCEPTION);
+    @GetMapping("/checkDuplicateId/{userId}") // 아이디 중복 확인
+    public ApiResponseDto<String> checkDuplicateId(@PathVariable String userId) {
+        return userService.checkDuplicateId(userId) ? ApiResponseDto.success(SuccessStatus.CREATE_ID_SUCCESS, userId)
+                : ApiResponseDto.error(ErrorStatus.CONFLICT_ID_EXCEPTION, userId);
     }
 
     @ApiOperation(value = "닉네임 중복 확인", notes = "해당 닉네임이 이미 존재한다면 true, 존재하지 않다면 false를 반환합니다.")
     @ApiImplicitParam(name = "nickname", value = "사용자가 생성한 닉네임")
     @GetMapping("/checkDuplicateNickname/{nickname}")
     public ApiResponseDto<String> checkDuplicateNickname(@PathVariable String nickname) {
-        boolean isDuplicateNickname = userService.checkDuplicateNickname(nickname);
-        return isDuplicateNickname ? ApiResponseDto.success(SuccessStatus.CREATE_NICKNAME_SUCCESS, nickname) : ApiResponseDto.error(ErrorStatus.CONFLICT_NICKNAME_EXCEPTION);
+        return userService.checkDuplicateNickname(nickname) ? ApiResponseDto.success(SuccessStatus.CREATE_NICKNAME_SUCCESS, nickname)
+                : ApiResponseDto.error(ErrorStatus.CONFLICT_NICKNAME_EXCEPTION, nickname);
     }
-
-//    @ApiOperation(value = "유저 날짜 가져오기", notes = "회원가입할 때 저장한 날짜를 가져옵니다.")
-//    @ApiImplicitParam(name = "userId", value = "사용자의 userId")
-//    @GetMapping("/user/{userId}/day")
-//    public ResponseEntity<String> getUserCustomDay(@PathVariable String userId) {
-//        Optional<User> optionalUser = userRepository.findById(userId);
-//        if (optionalUser.isPresent()) {
-//            User user = optionalUser.get();
-//            String date = user.getDate();
-//            if (date != null && !date.isEmpty()) {
-//                return ResponseEntity.ok(date);
-//            } else {
-//                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-//                        .body("Custom day not found for user " + userId);
-//            }
-//        } else {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-//                    .body("User not found with id " + userId);
-//        }
-//    }
 
     @ApiOperation(value = "유저 날짜 가져오기", notes = "회원가입할 때 저장한 날짜를 가져옵니다.")
     @ApiImplicitParam(name = "userId", value = "사용자의 userId")
